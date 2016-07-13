29a9a16ba80b12e91508e805f1075428002699c1
zhuangyan0728 <735758017@qq.com>
2016-07-13 19:58:15 +0800

    人事专员重复模块去除

--- src/test/java/com/thinkgem/jeesite/test/Test.java
+++ src/test/java/com/thinkgem/jeesite/test/Test.java
@@ -3,6 +3,9 @@ package com.thinkgem.jeesite.test;
 public class Test {
 
 	public static void main(String[] args) {
-		
+		String test = "type=1&param=谷歌";
+		String url = "type=1&param=sdfas谷dff";
+		url =url.substring(0,url.indexOf("param")+6)+ java.net.URLEncoder.encode(url.substring(url.indexOf("param")+6));
+		 System.out.println(url);
 	}
 }
